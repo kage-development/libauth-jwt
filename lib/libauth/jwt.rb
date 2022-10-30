0aa0052dff181dc7d76b49ffed49d8cb05fbe419
yuu <binsaki@craftorch.jp>
2022-10-30 13:32:07 +0900

    first commit

---
+++ lib/libauth/jwt.rb
@@ -0,0 +1,8 @@
+require "libauth/jwt/version"
+
+module Libauth
+  module Jwt
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/libauth/jwt_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Libauth::Jwt do
+  it "has a version number" do
+    expect(Libauth::Jwt::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
